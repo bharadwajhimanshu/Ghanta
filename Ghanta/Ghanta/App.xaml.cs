90de2e969d078edd74c6a255ac6e13237f83e372
Mayur Tendulkar <tendulkar@outlook.com>
2018-10-30 08:11:04 +0800

    TabbedPage added with Home & About. Removed MainPage

--- Ghanta/Ghanta/App.xaml.cs
+++ Ghanta/Ghanta/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ghanta.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace Ghanta
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomeTabbedPage();
         }
 
         protected override void OnStart()
---
+++ Ghanta/Ghanta/Views/HomeTabbedPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Ghanta.Views
+{
+    class HomeTabbedPage : TabbedPage
+    {
+        public HomeTabbedPage()
+        {
+            this.Children.Add(new GhantaPage());
+            this.Children.Add(new AboutPage());
+        }
+    }
+}
